22491da70f0c1392e14dba77b111faffa7e4000f
moali99 <mohammadali.mirzaee99@gmail.com>
2021-04-01 17:21:29 +0200

    some changes

--- index.php
+++ index.php
@@ -1,4 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body action="index.php" style="background-color: rgb(208, 216, 255);">
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 $con=mysqli_connect("localhost","mohammadali","","form") or die(mysqli_error());
 if((isset($_POST['submit']))){
 $firstname = $con->real_escape_string($_POST['FirstName']);
@@ -12,11 +25,14 @@ $sql="INSERT INTO contacts (FirstName, LastName, UserName, Email, City) VALUES (
   //Execute the query and returning a message
 if(!$result = $con->query($sql))
 {
-die('Error occured [' . $conn->error . ']');
+die('Error occured [' . $con->error . ']');
 }
 else
-   echo "Thank you for filling out your information!";
+   echo "<h1>Thank you for filling out your information!</h1>";
 }
-?>
 
+?>
+    
+</body>
+</html>
 
